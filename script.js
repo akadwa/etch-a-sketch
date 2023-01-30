7115c57c60abc9cf75424320c92f51841fddfd55
akadwa <kadazh123@gmail.com>
2023-01-30 22:25:00 +0200

    Add styles to slider and buttons

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
+// QUERY SELECTORS
+const grid = document.querySelector('.grid');
+const slider = document.querySelector('.slider');
 
+// DEFAULT GRID SIZE
+let gridSize = 16;
+
+
+
+// FUNCTION to add cells to the grid
+function addCells() {
+    for (i = 0; i < gridSize; i++){
+        const row = document.createElement('div');
+        row.classList.add('row')
+        grid.appendChild(row);
+        for (n = 0; n < gridSize; n++) {
+            const column = document.createElement('div');
+            column.classList.add('column');
+            row.appendChild(column);
+        }  
+    }
+}
+
+addCells();
 
