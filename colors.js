93d997ea47302386fb782e5669f32b90007be318
akadwa <kadazh123@gmail.com>
2023-01-31 15:33:27 +0200

    Copy file contents of index files to corresponding colos files

---
+++ colors.js
@@ -0,0 +1,51 @@
+// QUERY SELECTORS
+const grid = document.querySelector('.grid');
+const slider = document.querySelector('.slider');
+const apply = document.querySelector('#applyButton');
+const reset = document.querySelector('#resetButton');
+const displaySize = document.querySelector('#displaySize');
+
+// DEFAULT GRID SIZE
+let gridSize = 16;
+
+// FUNCTION to add cells to the grid as well as EVENT LISTENER to change background color on mouseover
+function addCells() {
+    for (i = 0; i < gridSize; i++){
+        const row = document.createElement('div');
+        row.classList.add('row')
+        grid.appendChild(row);
+        for (n = 0; n < gridSize; n++) {
+            const column = document.createElement('div');
+            column.classList.add('column');
+            row.appendChild(column);
+            // EVENT LISTENER for mouseover on individual cells
+            column.addEventListener('mouseover', () => {
+                column.style.backgroundColor = "black";
+                column.style.opacity = (parseFloat(column.style.opacity) || 0) + 0.25;
+            })
+        }  
+    }
+};
+
+// EVENT LISTENER for click on apply button
+apply.addEventListener('click', () => {
+    // FUNCTION to apply the selected value from the slider to the grid size and adjust it accordingly 
+    grid.innerHTML = " ";
+    gridSize = slider.value;
+    addCells();
+});
+
+// EVENT LISTENER for click on reset button
+reset.addEventListener('click', () => {
+    grid.innerHTML = " ";
+    addCells();
+})
+
+// EVENT LISTENER for the slider input
+//returns the new range in the displaySize div
+slider.addEventListener('mouseup', () => {
+    displaySize.innerHTML = ' ';
+    displaySize.innerHTML = slider.value;
+})
+
+addCells();
